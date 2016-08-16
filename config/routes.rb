025231f8dcd030b628a13503fc29faf0c4d61cb3
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-16 22:43:10 +0200

    adds posts controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
+  resources :posts
   root controller: :homepage, action: :index
 end
